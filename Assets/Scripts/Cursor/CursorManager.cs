d2ff70de61929ebc9f7251a554c34d41b6a1ac7c
kongfan <kongfan@dxx.cn>
2022-09-29 19:37:01 +0800

    feat: scene transition

---
+++ Assets/Scripts/Cursor/CursorManager.cs
@@ -0,0 +1,34 @@
+﻿using Transition;
+using UnityEngine;
+
+namespace Cursor
+{
+    public class CursorManager : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var clickedCollider = CheckColliderAtMousePosition();
+                if (clickedCollider) ClickAction(clickedCollider.gameObject);
+            }
+        }
+
+        private void ClickAction(GameObject clickObject)
+        {
+            switch (clickObject.tag)
+            {
+                case "Teleport":
+                    var teleport = clickObject.GetComponent<Teleport>();
+                    teleport.TeleportToScene();
+                    break;       
+            }
+        }
+
+        private Collider2D CheckColliderAtMousePosition()
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            return Physics2D.OverlapPoint(mouseWorldPos);
+        }
+    }
+}
---
+++ Assets/Scripts/Transition/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Transition
+{
+    public class Teleport : MonoBehaviour
+    {
+        public string sceneFrom;
+        public string sceneToGo;
+
+        public void TeleportToScene()
+        {
+            TransitionManager.Instance.Transition(sceneFrom, sceneToGo);
+        }
+    }
+}
